d06788b219a5af6980f744ea9aebd1aa42e61658
MouadCherrat <mouad19cherrat@gmail.com>
2024-10-31 21:59:01 +0100

    first commit

---
+++ src/main/java/com/example/jaxrsjersey/JaxRsJerseyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaxrsjersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxRsJerseyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaxRsJerseyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jaxrsjersey/JaxRsJerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaxrsjersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxRsJerseyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
